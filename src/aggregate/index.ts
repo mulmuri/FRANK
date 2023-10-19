807ea79aeecc634c6223d97e37a03f99d3dc6b5f
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-10-19 19:08:43 +0900

    create index.ts

---
+++ src/aggregate/index.ts
@@ -0,0 +1,11 @@
+import { BasicRankAggs, BasicRankAggsAsc, BasicRankAggsDesc } from "./basic";
+export { BasicRankAggs, BasicRankAggsAsc, BasicRankAggsDesc };
+
+import { OptimizedRankAggs, OptimizedRankAggsAsc, OptimizedRankAggsDesc } from "./optimized";
+export { OptimizedRankAggs, OptimizedRankAggsAsc, OptimizedRankAggsDesc };
+
+import { IRankAggs } from "./interface";
+export { IRankAggs };
+
+import ColumnAggs from "./column";
+export { ColumnAggs };
