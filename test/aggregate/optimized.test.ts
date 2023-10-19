087ddbe951a1cd899319da5387093043d4c0b338
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-10-19 15:59:33 +0900

    write optimized rank aggs test code

---
+++ test/aggregate/optimized.test.ts
@@ -0,0 +1,72 @@
+import { OptimizedRankAggsAsc, OptimizedRankAggsDesc } from "../../src/aggregate";
+
+
+describe("OptimizedRankAsc", () => {
+
+    let rankAggs = new OptimizedRankAggsAsc(0, 31);
+
+    it("should be initialized", () => {
+        expect(rankAggs.rank(0)).toBe(0);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(0);
+    });
+
+    it("should return proper rank after increase", () => {
+        rankAggs.inc(1);
+        expect(rankAggs.rank(0)).toBe(0);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(1);
+        expect(rankAggs.rank(3)).toBe(1);
+
+        rankAggs.inc(2);
+        expect(rankAggs.rank(0)).toBe(0);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(1);
+        expect(rankAggs.rank(3)).toBe(2);
+
+        expect(rankAggs.exists(1)).toBe(true);
+        expect(rankAggs.size()).toBe(2);
+    });
+
+    it("should return proper rank after decrease", () => {
+        rankAggs.dec(1);
+        expect(rankAggs.rank(0)).toBe(0);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(0);
+        expect(rankAggs.rank(3)).toBe(1);
+
+        expect(rankAggs.exists(1)).toBe(false);
+        expect(rankAggs.size()).toBe(1);
+    });
+});
+
+
+describe("OptimizedRankDesc", () => {
+
+    let rankAggs = new OptimizedRankAggsDesc(0, 31);
+
+    it("should be initialized", () => {
+        expect(rankAggs.rank(0)).toBe(0);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(0);
+    });
+
+    it("should return proper rank after updated", () => {
+        rankAggs.inc(1);
+        expect(rankAggs.rank(0)).toBe(1);
+        expect(rankAggs.rank(1)).toBe(0);
+        expect(rankAggs.rank(2)).toBe(0);
+
+        rankAggs.inc(2);
+        expect(rankAggs.rank(0)).toBe(2);
+        expect(rankAggs.rank(1)).toBe(1);
+        expect(rankAggs.rank(2)).toBe(0);
+    });
+
+    it("should return proper rank after decrease", () => {
+        rankAggs.dec(1);
+        expect(rankAggs.rank(0)).toBe(1);
+        expect(rankAggs.rank(1)).toBe(1);
+        expect(rankAggs.rank(2)).toBe(0);
+    });
+});
