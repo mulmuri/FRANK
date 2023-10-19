5304bd7849aabdf2aaa01e7b166c6b769b0bd0d6
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-10-19 19:51:30 +0900

    create indexset

---
+++ src/set/indexset.ts
@@ -0,0 +1,30 @@
+import { Index, Key } from "../model/basic";
+
+
+
+class IndexSet<K extends Key, I extends Index> {
+
+    map: Map<K, I>;
+
+    constructor() {
+        this.map = new Map();
+    }
+
+    insert(key: K, index: I): void {
+        this.map.set(key, index);
+    }
+
+    remove(key: K): void {
+        this.map.delete(key);
+    }
+
+    update(key: K, index: I): void {
+        this.map.set(key, index);
+    }
+
+    index(key: K): I {
+        return this.map.get(key)!;
+    }
+}
+
+export default IndexSet;
